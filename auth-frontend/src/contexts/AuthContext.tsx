8034692aaf5248b9070a4877c3fd53b41a64c11b
Jorge de Souza <jorgejrdj1@gmail.com>
2022-01-31 20:34:59 -0300

    Recuperando os estado da autenticação dentro de nookies

--- auth-frontend/src/contexts/AuthContext.tsx
+++ auth-frontend/src/contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from 'next/router';
 import { api } from "../services/api";
 
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 type SingInCredentials = {
     email: string;
@@ -30,6 +30,18 @@ export const AuthContext = createContext({} as AuthContextData)
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data;
+
+                setUser({ email, permissions, roles })
+            })
+        }
+    }, [])
 
     async function signIn({ email, password }: SingInCredentials) {
         try {
